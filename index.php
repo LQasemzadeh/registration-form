ce3bd165de7c2508a6168df5f6a0ccff76f99301
lqasemzadeh <lqasemzadeh@gmail.com>
2023-06-30 20:06:06 +0330

    registrationform in php

--- index.php
+++ index.php
@@ -1,128 +1,143 @@
-<?php
-// Configuration for database connection
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "registration";
-
-// Create a database connection
-$conn = mysqli_connect($host, $username, $password, $dbname);
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-// Function to sanitize input data
-function sanitize($data) {
-    global $conn;
-    $data = trim($data);
-    $data = mysqli_real_escape_string($conn, $data);
-    return $data;
-}
-
-// Initialize variables
-$name = $last_name = $email = $mobile = $password = $picture = "";
-
-// Process the form submission
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $name = sanitize($_POST["name"]);
-    $last_name = sanitize($_POST["last_name"]);
-    $email = sanitize($_POST["email"]);
-    $mobile = sanitize($_POST["mobile"]);
-    $password = sanitize($_POST["password"]);
-    $picture = $_FILES["picture"]["name"];
-
-    // Upload the picture file
-    $targetDir = "uploads/"; // Directory to store uploaded pictures
-    $targetFile = $targetDir . basename($picture);
-    move_uploaded_file($_FILES["picture"]["tmp_name"], $targetFile);
-
-    // Insert the registration data into the database
-    $sql = "INSERT INTO users (name, last_name, email, mobile, password, picture)
-            VALUES ('$name', '$last_name', '$email', '$mobile', '$password', '$picture')";
-    if (mysqli_query($conn, $sql)) {
-        $message = "Your registration completed successfully.";
-    } else {
-        $message = "Error: " . mysqli_error($conn);
-    }
-}
-?>
-
 <!DOCTYPE html>
-<html>
+<html lang="ar" dir="rtl">
 <head>
-    <title>Registration Page</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Sign Up</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/gh/rastikerdar/vazir-font@v30.1.0/dist/font-face.css" rel="stylesheet" type="text/css" />
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <style>
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            padding: 0;
-            background-color: #f1f1f1;
+        @font-face {
+            font-family: Vazir;
+            src: url('Vazir-Regular.eot');
+            src: url('Vazir-Regular.eot?#iefix') format('embedded-opentype'),
+            url('Vazir-Regular.woff2') format('woff2'),
+            url('Vazir-Regular.woff') format('woff'),
+            url('Vazir-Regular.ttf') format('truetype');
+            font-weight: normal;
+            font-style: normal;
+        }
+        @font-face {
+            font-family: Vazir;
+            src: url('Vazir-Bold.eot');
+            src: url('Vazir-Bold.eot?#iefix') format('embedded-opentype'),
+            url('Vazir-Bold.woff2') format('woff2'),
+            url('Vazir-Bold.woff') format('woff'),
+            url('Vazir-Bold.ttf') format('truetype');
+            font-weight: bold;
+            font-style: normal;
+        }
+        @font-face {
+            font-family: Vazir;
+            src: url('Vazir-Black.eot');
+            src: url('Vazir-Black.eot?#iefix') format('embedded-opentype'),
+            url('Vazir-Black.woff2') format('woff2'),
+            url('Vazir-Black.woff') format('woff'),
+            url('Vazir-Black.ttf') format('truetype');
+            font-weight: 900;
+            font-style: normal;
+        }
+        @font-face {
+            font-family: Vazir;
+            src: url('Vazir-Medium.eot');
+            src: url('Vazir-Medium.eot?#iefix') format('embedded-opentype'),
+            url('Vazir-Medium.woff2') format('woff2'),
+            url('Vazir-Medium.woff') format('woff'),
+            url('Vazir-Medium.ttf') format('truetype');
+            font-weight: 500;
+            font-style: normal;
+        }
+        @font-face {
+            font-family: Vazir;
+            src: url('Vazir-Light.eot');
+            src: url('Vazir-Light.eot?#iefix') format('embedded-opentype'),
+            url('Vazir-Light.woff2') format('woff2'),
+            url('Vazir-Light.woff') format('woff'),
+            url('Vazir-Light.ttf') format('truetype');
+            font-weight: 300;
+            font-style: normal;
+        }
+        @font-face {
+            font-family: Vazir;
+            src: url('Vazir-Thin.eot');
+            src: url('Vazir-Thin.eot?#iefix') format('embedded-opentype'),
+            url('Vazir-Thin.woff2') format('woff2'),
+            url('Vazir-Thin.woff') format('woff'),
+            url('Vazir-Thin.ttf') format('truetype');
+            font-weight: 100;
+            font-style: normal;
+        }
+    </style>
+    <style>
+        body{
+            font-family: 'Vazir', sans-serif;
         }
 
-        form {
-            background-color: #ffffff;
-            padding: 20px;
-            border-radius: 5px;
-            max-width: 400px;
-            width: 100%;
-        }
-
-        label, input, select {
-            display: block;
-            margin-bottom: 10px;
-        }
-
-        input[type="submit"] {
-            background-color: #4CAF50;
-            color: white;
-            padding: 10px;
-            border: none;
-            cursor: pointer;
-        }
-
-        input[type="submit"]:hover {
-            background-color: #45a049;
-        }
     </style>
 </head>
 <body>
-<h2 style="text-align: center;">Registration Page</h2>
-<form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>" enctype="multipart/form-data">
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name" required>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+    	
+    	<form class="shadow w-450 p-3" 
+    	      action="php/signup.php" 
+    	      method="post"
+    	      enctype="multipart/form-data">
 
-    <label for="last_name">Last Name:</label>
-    <input type="text" id="last_name" name="last_name" required>
+    		<h4 class="display-4  fs-1">اطلاعات کاربر</h4><br>
+    		<?php if(isset($_GET['error'])){ ?>
+    		<div class="alert alert-danger" role="alert">
+			  <?php echo $_GET['error']; ?>
+			</div>
+		    <?php } ?>
 
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required>
+		    <?php if(isset($_GET['success'])){ ?>
+    		<div class="alert alert-success" role="alert">
+			  <?php echo $_GET['success']; ?>
+			</div>
+		    <?php } ?>
+		  <div class="mb-3">
+		    <label class="form-label">نام و نام خانوادگی</label>
+		    <input type="text" 
+		           class="form-control"
+		           name="fname"
+		           value="<?php echo (isset($_GET['fname']))?$_GET['fname']:"" ?>">
+		  </div>
 
-    <label for="mobile">Mobile:</label>
-    <input type="text" id="mobile" name="mobile" required>
+		  <div class="mb-3">
+		    <label class="form-label">ایمیل</label>
+		    <input type="email"
+		           class="form-control"
+		           name="uname"
+		           value="<?php echo (isset($_GET['uname']))?$_GET['uname']:"" ?>">
+		  </div>
 
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required>
+            <div class="mb-3">
+                <label class="form-label">شماره همراه</label>
+                <input type="text"
+                       class="form-control"
+                       name="uname"
+                       value="<?php echo (isset($_GET['uname']))?$_GET['uname']:"" ?>">
+            </div>
 
-    <label for="picture">Picture:</label>
-    <input type="file" id="picture" name="picture" required>
+		  <div class="mb-3">
+		    <label class="form-label">کلمه عبور</label>
+		    <input type="password" 
+		           class="form-control"
+		           name="pass">
+		  </div>
 
-    <input type="submit" value="Register">
-</form>
-
-<?php if (isset($message)) { ?>
-    <p><?php echo $message; ?></p>
-<?php } ?>
-
+		  <div class="mb-3">
+		    <label class="form-label">تصویر</label>
+		    <input type="file" 
+		           class="form-control"
+		           name="pp">
+		  </div>
+		  
+		  <button type="submit" class="btn btn-primary">ثبت نام</button>
+		  <a href="login.php" class="link-secondary">ورود</a>
+		</form>
+    </div>
 </body>
 </html>
-</html>
-
-<?php
-// Close the database connection
-mysqli_close($conn);
-?>
-
